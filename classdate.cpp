98b739cc9cb1e54343407c5f6f7173615c70d8a7
d4xDFl0w4r <d4xdfl0w4r@yandex.ru>
2024-02-27 23:17:47 +0300

    Changes in class Date

--- classdate.cpp
+++ classdate.cpp
@@ -1,51 +1,70 @@
 #include "classdate.h"
 
-Date::Date(int day, int month, int year) {
+Date::Date(unsigned int day, unsigned int month, unsigned int year) {
     this->day = day;
     this->month = month;
     this->year = year;
 }
 
-int Date::GetDay() {
+unsigned int Date::GetDay() {
     return day;
 }
 
-void Date::SetDay(int day) {
-    this->day = day;
+void Date::SetDay(unsigned int day) {
+    if (month == 2) {
+        if (day <= 28) {
+            this.day = day;
+        } else if (day == 29 && this.year % 4 == 0) {
+            this.day = day;
+        }
+
+    } else if ((month % 2 == 1 && month <= 7) || (month % 2 == 0 && month >= 8)) {
+        if (day <= 31) {
+            this.day = day;
+        }
+    } else {
+        if (day <= 30) {
+
+        }
+    }
 }
 
-int Date::GetMonth() {
+unsigned int Date::GetMonth() {
     return month;
 }
 
-void Date::SetMonth(int month) {
-    this->month = month;
+void Date::SetMonth(unsigned int month) {
+    if (month >= 1 && month <= 12) {
+        this->month = month;
+    }
 }
 
-int Date::GetYear() {
+unsigned int Date::GetYear() {
     return year;
 }
 
-void Date::SetYear(int year) {
-    this->year = year;
+void Date::SetYear(unsigned int year) {
+    if (year >= 2000 && year <= 2024) {
+        this->year = year;
+    }
 }
 
 
 Date::CompareDates(Date* firstDate, Date* secondDate) {
     if (firstDate->year > secondDate->year) {
-        return 1;
-    } else if (firstDate->year < secondDate->year) {
         return -1;
+    } else if (firstDate->year < secondDate->year) {
+        return 1;
     } else {
         if (firstDate->month > secondDate->month) {
-            return 1;
-        } else if (firstDate->month < secondDate->month) {
             return -1;
+        } else if (firstDate->month < secondDate->month) {
+            return 1;
         } else {
             if (firstDate->day > secondDate->day) {
-                return 1;
-            } else if (firstDate->day < secondDate->day) {
                 return -1;
+            } else if (firstDate->day < secondDate->day) {
+                return 1;
             } else {
                 return 0;
             }
--- classdate.h
+++ classdate.h
@@ -3,22 +3,22 @@
 
 class Date {
 public:
-    Date(int day, int month, int year);
+    Date(unsigned int day, unsigned int month, unsigned int year);
     ~Date();
 
-    int GetDay();
-    void SetDay(int day);
-    int GetMonth();
-    void SetMonth(int month);
-    int GetYear();
-    void SetYear(int year);
+    unsigned int GetDay();
+    void SetDay(unsigned int day);
+    unsigned int GetMonth();
+    void SetMonth(unsigned int month);
+    unsigned int GetYear();
+    void SetYear(unsigned int year);
 
     static int CompareDates(Date* firstDate, Date* secondDate);
 
 private:
-    int day;
-    int month;
-    int year;
+    unsigned int day;
+    unsigned int month;
+    unsigned int year;
 };
 
 #endif // CLASSDATE_H
